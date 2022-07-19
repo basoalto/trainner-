31b15016cf2657762a71c07696967ebc22363a1a
Lucas <lucasbasoaltocontreras@gmail.com>
2022-07-18 22:03:42 -0400

    primer commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from  'next/head'
+import styled from "@emotion/styled";
+
+const Heading =styled.h1`
+color: red;
+`;
+
+
+const Home = (
+) => (
+  <div>
+    <Heading>buenos dias</Heading>
+  </div>
+)
+
+export default Home 
